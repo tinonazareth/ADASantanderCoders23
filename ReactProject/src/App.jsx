059bce333972e0406b1fa5bba7a4fc0ce905c472
tinonazareth <zingidu@gmail.com>
2023-11-09 18:32:31 -0300

    docs: pasta react

---
+++ ReactProject/src/App.jsx
@@ -0,0 +1,27 @@
+import MeuComponente from "./componets/MeuComponente"
+function App() {
+  
+  return (
+    <div>
+      <h1>Olá Mundo</h1>
+      <MeuComponente />
+      <Botao conteudo= 'Clique me'/>
+      <Botao conteudo= 'Mais uma vez' />
+      <Botao conteudo= 'Novamente'/>
+      {/* /* <Elemento imagem='url' titulo='exemplo titulo' subtitulo='exemplo subtitulo'  ( serve pra reutilizar com props apenas trocando as propiedades />*/}
+    </div>
+    
+  )
+}
+
+function Botao(props) {
+  return (
+    <button>{props.conteudo}</button>
+     
+    
+  )
+}
+
+
+
+export default App
---
+++ ReactProject/src/componets/MeuComponente.jsx
@@ -0,0 +1,9 @@
+export default function MeuComponente() {
+    return (
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit vero quos sed quis ipsum totam eius. Quas earum, aliquid harum reiciendis saepe ad illum sit, vel, quae ex nulla!
+      </p>
+    )
+  }
+
+  
